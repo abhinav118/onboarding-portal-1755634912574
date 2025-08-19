4ee8a19f1f3b83dcf6c04fe08dfdc2ace0939b6b
Abhi <abhinav118@gmail.com>
2025-08-19 16:22:00 -0400

    Add components/InfoCard.tsx

---
+++ components/InfoCard.tsx
@@ -0,0 +1,14 @@
+interface InfoCardProps {
+  title?: any
+  description?: any
+  icon?: any
+  action?: any
+}
+
+export default function InfoCard({ title, description, icon, action }: InfoCardProps) {
+  return (
+    <div className="">
+      {/* InfoCard component content */}
+    </div>
+  )
+}
