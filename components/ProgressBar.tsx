17396875a2544a3d30fb13e6e970b928d6b495f1
Abhi <abhinav118@gmail.com>
2025-08-19 16:21:59 -0400

    Add components/ProgressBar.tsx

---
+++ components/ProgressBar.tsx
@@ -0,0 +1,13 @@
+interface ProgressBarProps {
+  value?: any
+  max?: any
+  showLabel?: any
+}
+
+export default function ProgressBar({ value, max, showLabel }: ProgressBarProps) {
+  return (
+    <div className="">
+      {/* ProgressBar component content */}
+    </div>
+  )
+}
